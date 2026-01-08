0434c0c0fc33dedeae666e7fe3b7bb6750f9b3b0
santhoshh005 <saisanthosh05012006@gmail.com>
2026-01-08 18:36:49 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("library management system")
+students = []
+
+sid = input("Enter Student ID: ")
+name = input("Enter Name: ")
+dept = input("Enter Department: ")
+
+students.append([sid, name, dept])
+
+print("Student Registered Successfully")
+print("Registered Students:", students)
